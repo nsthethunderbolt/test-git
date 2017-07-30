1898cab70fdb0380ad37f0f382886afe0a567983
hodns <nsthethunderbolt@gmail.com>
2017-07-30 15:48:54 +0530

    second commit: test changed, second added

---
+++ src/Second.java
@@ -0,0 +1,7 @@
+/**
+ * Created by hodns on 30/7/17.
+ */
+public class Second {
+    // this is second class to commit.
+    // nothing is pushed yet.
+}
--- src/Test.java
+++ src/Test.java
@@ -4,5 +4,7 @@
 public class Test {
     public static void main(String[] args){
         System.out.println("This is the first file and first commit");
+        // adding this comment
+
     }
 }
